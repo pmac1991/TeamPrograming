07343f5c021fe56dac0bb8a361f13e2c4093f806
pmac1991 <piomac11@gmail.com>
2015-04-29 21:29:48 +0200

    Adding missing

--- src/main/java/totalservice/servlets/FirmsServlet.java
+++ src/main/java/totalservice/servlets/FirmsServlet.java
@@ -1,25 +1,22 @@
 package totalservice.servlets;
 
-
-
-
-
-	import java.io.IOException;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import totalservice.data.base.DataProviderImplement;
 		
 public class FirmsServlet  extends HttpServlet {
 		
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
-			response.setContentType("text/html");			
-			
-			
-
-		      // Actual logic goes here.						
+			DataProviderImplement dpi = new DataProviderImplement();
+			response.setContentType("text/html");
+			request.setAttribute("firms", dpi.selectFirm(""));
+		      						
 			request.getRequestDispatcher("/WEB-INF/AllFirms.jsp").forward(request, response);
 		}
 
