c778a5f8d883f5ae82af90a3090acb2b3ceccebf
MarchenkoIvan <ForWork.I.V@gmail.com>
2015-06-16 23:27:55 +0200

    Email sender
    Email sender

--- src/main/java/totalservice/servlets/LoginServlet.java
+++ src/main/java/totalservice/servlets/LoginServlet.java
@@ -14,8 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			// TODO Auto-generated method stub
 			response.setContentType("text/html");			
-			
-			
+			String error = request.getParameter("loginError");
+			request.setAttribute("loginError", error);
 
 		      // Actual logic goes here.
 			request.setAttribute("title", "Login");			
@@ -26,7 +26,15 @@ import javax.servlet.http.HttpServletResponse;
 		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 		 */
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
+			final HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String login = httpRequest.getParameter("login");
+			String pass = httpRequest.getParameter("password");
+			if("admin".equals(login) && "admin".equals(pass)){
+				response.sendRedirect("/TotalService/firms");
+			} else{
+				request.setAttribute("loginError", "Invalid login or password");	
+				response.sendRedirect("/TotalService/login");
+			}
 		}
 
 
